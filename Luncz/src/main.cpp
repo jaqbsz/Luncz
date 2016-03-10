6beb75a81f924be4f322a0892235a4739c2d62bb
jaqbsz <ja.qb.sz@gmail.com>
2016-03-10 22:53:28 +0100

    SQLiteCpp wrapper included

--- Luncz/src/main.cpp
+++ Luncz/src/main.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
 #include <typeinfo>
+#include "SQLite\sqlite3.h"
+
 #include "Zamawiacz.h"
 #include "OrderList.h"
 #include "UserList.h"
+
 
 using namespace std;
 
@@ -12,43 +15,20 @@ const int ORDER_CNT = 10;
 
 int main(int argc, char **argv)
 {
-  User *u_table[USER_CNT];
-  for (int i = 0; i < USER_CNT;i++){u_table[i] = NULL;}
+  sqlite3 *db;
+  char *zErrMsg = 0;
+  int rc;
 
-  cout<<"Luncz v0.1"<<endl;
+  rc = sqlite3_open("test.db", &db);
 
-  UserList u_list = UserList(u_table, USER_CNT);
-
-  u_list.AddUser("Marian", "Bibi");
-  u_list.AddUser("Jurek", "Zaraz");
-  u_list.AddUser("Janek", "Beza", true);
-
-  u_list.ListUsers();
-  u_list.DeleteUser(2);
-  u_list.ListUsers();
-
-  OrderList o_list = OrderList(ORDER_CNT);
-
-  try
+  if (rc)
   {
-    o_list.AddNewOrder(u_list.GetUser(1), 13, 23);
-    o_list.AddNewOrder(u_list.GetUser(3), 14, 24);
-    o_list.AddNewOrder(u_list.GetUser(5), 14, 24);
+    cout<<"Failed!!"<<endl;
   }
-  catch (int id)
+  else
   {
-    cout<<"\n!Failed to add order for user ID "<<id<<"!"<<endl;
+    cout<<"Opened database successfully\n";
   }
 
-  Zamawiacz * zam = dynamic_cast<Zamawiacz*>(u_list.GetUser(u_list.GetZamawiaczId()));
-
-  if ( zam )
-    zam->MakeOrder(&o_list);
-
-  o_list.DeleteOrder(u_list.GetUser(1));
-
-  if ( zam )
-    zam->MakeOrder(&o_list);
-
-  cout<<"End"<<endl;
+  sqlite3_close(db);
 }
