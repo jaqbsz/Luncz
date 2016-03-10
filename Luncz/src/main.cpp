37e315328c1f80d22599462ee4d2ef5b96053860
jaqbsz <ja.qb.sz@gmail.com>
2016-03-10 23:10:37 +0100

    Working Hello World for SQLite

--- Luncz/src/main.cpp
+++ Luncz/src/main.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <typeinfo>
 #include "SQLite\sqlite3.h"
+#include "SQLiteCpp\SQLiteCpp.h"
 
 #include "Zamawiacz.h"
 #include "OrderList.h"
 #include "UserList.h"
-
 
 using namespace std;
 
@@ -15,20 +15,8 @@ const int ORDER_CNT = 10;
 
 int main(int argc, char **argv)
 {
-  sqlite3 *db;
-  char *zErrMsg = 0;
-  int rc;
+  cout << "SQlite3 version " << SQLITE_VERSION << endl;
+  cout << "SQliteC++ version " << SQLITECPP_VERSION << endl;
 
-  rc = sqlite3_open("test.db", &db);
 
-  if (rc)
-  {
-    cout<<"Failed!!"<<endl;
-  }
-  else
-  {
-    cout<<"Opened database successfully\n";
-  }
-
-  sqlite3_close(db);
 }
